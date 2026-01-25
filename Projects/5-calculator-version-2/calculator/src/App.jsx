0369290bcf6a33fbcfe1621cf6c3c718c541cabe
Mohammad Ahmed Khan <ahmedkhan@Mohammads-MacBook-Air.local>
2026-01-25 20:37:06 +0530

    calc-2

---
+++ Projects/5-calculator-version-2/calculator/src/App.jsx
@@ -0,0 +1,19 @@
+import styles from "./App.module.css";
+import Display from "./Display.jsx";
+import ButtonsContainer from "./ButtonsContainer.jsx";
+import { useState } from "react";
+
+function App() {
+
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => console.log(buttonText);
+  
+  return(
+  <div className={styles.calculator}>
+    <Display displayValue = {calVal}/>
+    <ButtonsContainer onButtonClick={onButtonClick}/>
+  </div> 
+  );
+}
+
+export default App

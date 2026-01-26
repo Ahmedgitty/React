d42d81ad617c844b785f81b2becb294822c5c3c7
Mohammad Ahmed Khan <ahmedtariq1812@gmail.com>
2026-01-26 12:57:13 +0530

    calculator done

---
+++ Projects/6-todo-app-version-three/src/App.jsx
@@ -0,0 +1,39 @@
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/AppName";
+import "./App.css";
+import WelcomeMessage from "./Components/WelcomeMessage";
+import TodoItems from "./Components/TodoItems";
+import { useState } from "react";
+
+function App() {
+
+ /* const intitalTodoItems = [
+    { dueDate: "4/10/2024", name: "Buy milk" },
+    { dueDate: "4/10/2024", name: "Go To Gym" },
+    { dueDate: "4/11/2024", name: "Read a book" },
+  ]; */
+
+  const[todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, ItemDueDate) => {
+    console.log(`New Item added: ${itemName} Date: ${ItemDueDate}`);
+    const newTodoItems = [...todoItems, {name: itemName, dueDate: ItemDueDate}];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    console.log(`Item Deleted: ${todoItemName}`);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem}/>
+      {todoItems.length === 0 && <WelcomeMessage/>}
+      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem}/>
+      
+    </center>
+  );
+}
+
+export default App;
